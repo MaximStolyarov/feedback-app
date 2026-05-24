e9b0e962596e9e8e4e0fb9c48f8ce44a46e42ab0
Maxim Stolyarov <Maxfan900@gmail.com>
2026-05-24 18:05:54 +0300

    Add default themes to database

--- feedback-backend/WebApi/Data/AppDbContext.cs
+++ feedback-backend/WebApi/Data/AppDbContext.cs
@@ -12,5 +12,15 @@ namespace WebApi.Data
         public DbSet<Message> Messages { get; set; }
         public DbSet<MessageTheme> MessageThemes { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MessageTheme>().HasData(
+                new MessageTheme { Id = 1, Name = "Техподдержка" },
+                new MessageTheme { Id = 2, Name = "Продажи" },
+                new MessageTheme { Id = 3, Name = "Другое" },
+                new MessageTheme { Id = 4, Name = "Ещё один пункт" }
+            );
+        }
+
     }
 }
