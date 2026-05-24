30290b280b7368e6a695fd230d08b3e209cad344
Maxim Stolyarov <Maxfan900@gmail.com>
2026-05-24 18:11:20 +0300

    Add migrations

---
+++ feedback-backend/WebApi/Migrations/20260524151039_SeedMessageThemes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMessageThemes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MessageThemes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Техподдержка" },
+                    { 2, "Продажи" },
+                    { 3, "Другое" },
+                    { 4, "Ещё один пункт" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MessageThemes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MessageThemes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "MessageThemes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "MessageThemes",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
