b69a4707f9aac701a5e8d218de17ed68c28bcddb
Maxim Stolyarov <Maxfan900@gmail.com>
2026-05-24 18:25:48 +0300

    Update CORS

--- feedback-backend/WebApi/Program.cs
+++ feedback-backend/WebApi/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "https://feedback-app-iota-seven.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod());
 });
